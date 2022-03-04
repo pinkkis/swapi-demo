1b024b449ff9d3c39068c5b63552837a319e45b1
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2022-03-04 14:12:44 +0000

    feat: make updates to scaffolded app and start store

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const API_BASE = 'https://swapi.dev/api/';
+export const API_PEOPLE = `${API_BASE}people/`;
+export const API_PLANETS = `${API_BASE}planets/`;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,34 @@
-import { createStore } from 'vuex';
+import { createStore, createLogger } from 'vuex';
 
 export default createStore({
-	state: {},
-	getters: {},
-	mutations: {},
-	actions: {},
-	modules: {},
+	state: {
+		loading: false,
+		people: [],
+		planets: [],
+	},
+	getters: {
+		people(state) {
+			return state.people;
+		},
+		planets(state) {
+			return state.planets;
+		},
+		planet: (state) => (id) => state.planets.find((p) => p.id === id),
+	},
+	mutations: {
+		addPeople(state, people) {
+			state.people = [...state.people, ...people];
+		},
+		addPlanets(state, planets) {
+			state.planets = [...state.planets, ...planets];
+		},
+		setLoading(state, loading) {
+			state.loading = loading;
+		},
+	},
+	actions: {
+		getPeople() {},
+		getPlanets() {},
+	},
+	plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : [],
 });
